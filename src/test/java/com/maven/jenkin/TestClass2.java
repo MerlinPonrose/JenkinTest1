cbc8b6a7a0c9f888b17a7c9fca673cd307e4f33d
unknown <mrlnrose6@gmail.com>
2023-11-30 09:16:54 +0530

    suitexml

---
+++ src/test/java/com/maven/jenkin/TestClass2.java
@@ -0,0 +1,51 @@
+package com.maven.jenkin;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TestClass2 {
+	
+
+	public static WebDriver driver;
+	
+	@BeforeTest
+	public void precondition1()
+	{
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		
+		driver.get("https://demo.automationtesting.in/Register.html"); // to open URL
+		
+	}
+	
+	@Test
+	public void test()
+	{
+		System.out.println("new test 2");
+		org.openqa.selenium.WebElement addr =		driver.findElement(By.tagName("textarea"));
+		
+		addr.click();
+		addr.clear();
+		addr.sendKeys("Perungudi");
+		
+		WebElement frstname = driver.findElement(By.xpath("//input[@placeholder='First Name']"));
+		frstname.click();
+		frstname.sendKeys("JAVA");
+			
+	}
+	
+	@AfterTest
+	public void closebrowser()
+	{
+		driver.close();
+	}
+
+}
